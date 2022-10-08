c1eeca19a0d832a7bd743e435cc21c9005f78805
rojinn <tech.rojinn@gmail.com>
2022-10-08 21:17:53 +0545

    Fixes

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import { View, Text, StyleSheet, StatusBar } from "react-native";
 import Header from "./src/component/header";
 import { colors } from "./src/global/styles";
 import SigninScreen from "./src/screens/authScreens/Signinscreen";
+import WelcomeScreen from "./src/screens/authScreens/SignInWelcomeScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barstyle="light-content" backgroundColor={colors.statusbar} />
-      <SigninScreen />
+      {/* <SigninScreen /> */}
+      <WelcomeScreen />
     </View>
   );
 }
