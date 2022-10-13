da8865183b167a5870f993acc5ec8f57a027a73c
ssthaisha <ssthaisha01@gmail.com>
2022-10-13 18:51:03 +0545

    Chef profiles

--- src/screens/authScreens/HomeScreen.js
+++ src/screens/authScreens/HomeScreen.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
-
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
   ScrollView,
+  FlatList,
+  Pressable,
+  Image,
 } from "react-native";
 import { Icon } from "react-native-elements";
 import Homeheader from "../../component/Homeheader";
@@ -13,13 +15,13 @@ import { colors, parameters } from "../../global/styles";
 
 export default function HomeScreen() {
   const [delivery, setdelivery] = useState(true);
+  const [indexCheck, setindexCheck] = useState("0");
 
   return (
     <View style={styles.container}>
       <Homeheader />
       <ScrollView stickyHeaderIndices={[0]} showsVerticalScrollIndicator={true}>
         <View>
-          {" "}
           <View
             style={{
               marginTop: 10,
@@ -57,26 +59,67 @@ export default function HomeScreen() {
             </TouchableOpacity>
           </View>
         </View>
-        <View>
-          <View style={{ flexDirection: "row" }}>
-            <Icon
-              type="material-community"
-              name="map-marker"
-              color={colors.grey1}
-              size={26}
-            />
-            <Text style={{ marginLeft: 5 }}>Gwarko Chowk</Text>
+
+        <View style={styles.filterView}>
+          <View style={styles.addressView}>
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingLeft: 20,
+              }}
+            >
+              <Icon
+                type="material-community"
+                name="map-marker"
+                color={colors.grey1}
+                size={26}
+              />
+              <Text style={{ marginLeft: 5 }}>Gwarko Chowk</Text>
+            </View>
+            <View style={styles.clockView}>
+              <Icon
+                type="material-community"
+                name="clock-time-four"
+                color={colors.grey1}
+                size={26}
+              />
+              <Text style={{ marginLeft: 5 }}>Now</Text>
+            </View>
           </View>
-          <View style={{ flexDirection: "row" }}>
+
+          <View>
             <Icon
               type="material-community"
-              name="clock-time-four"
+              name="tune"
               color={colors.grey1}
               size={26}
             />
-            <Text style={{ marginLeft: 5 }}>Now</Text>
           </View>
         </View>
+
+        <View>
+          <Text
+            style={{
+              color: colors.grey2,
+              paddingHorizontal: 10,
+              fontSize: 26,
+              fontWeight: "bold",
+              backgroundColor: colors.grey5,
+              paddingLeft: 20,
+            }}
+          >
+            Category
+          </Text>
+        </View>
+        <View></View>
+
+        {/* <View>
+            <FlatList
+            horizontal={true}
+            showsHorizontalScrollIndicator={false}
+
+        </View> */}
       </ScrollView>
     </View>
   );
@@ -94,5 +137,58 @@ const styles = StyleSheet.create({
   deliveryText: {
     marginLeft: 5,
     fontSize: 16,
+  },
+  filterView: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    marginHorizontal: 10,
+    marginVertical: 10,
+  },
+  clockView: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: 20,
+    backgroundColor: "#fefefe",
+    borderRadius: 15,
+    paddingHorizontal: 5,
+    marginRight: 20,
+  },
+  addressView: {
+    flexDirection: "row",
+    backgroundColor: colors.grey5,
+    borderRadius: 15,
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+    marginLeft: 20,
+    marginRight: 30,
+  },
+  smallCard: {
+    borderradius: 15,
+    backgroundColor: colors.grey5,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+  smallCardSelected: {
+    borderradius: 15,
+    backgroundColor: colors.buttons,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    width: 80,
+    margin: 10,
+    height: 100,
+  },
+  smallCardTextSelected: {
+    fontWeight: "bold",
+    color: colors.cardBackground,
+  },
+  smallCardText: {
+    fontWeight: "bold",
+    color: colors.grey2,
   },
 });
